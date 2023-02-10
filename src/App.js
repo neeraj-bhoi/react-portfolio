a5e57c7eb4e18147618af33ec6ded075906c9286
neeraj-bhoi <neerajbhoi2001@gmail.com>
2023-02-10 20:41:51 +0530

    skills and details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
+import MySkills from "./components/MySkills";
+
 
 function App() {
   return (
     <>
       <Hero />
+      <AboutMe />
+      <MySkills />
     </>
   );
 }
